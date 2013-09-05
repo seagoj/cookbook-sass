4a28393492744eceb5cc0a30c0c8ceb4f0dbff0c
Jeremy Seago <seagoj@gmail.com>
2013-09-05 14:01:08 -0500

    Adds attributes to recipe

---
+++ attributes/default.rb
@@ -0,0 +1,3 @@
+default[:sass][:user] = "root"
+default[:sass][:group]= "root"
+default[:sass][:lib] = "/vagrant/lib"
--- recipes/default.rb
+++ recipes/default.rb
@@ -1,38 +1,23 @@
-lib="/vagrant/lib"
-sassPath="#{lib}/sass"
-
 include_recipe 'ruby'
 
 execute 'install-sass' do
     command 'gem install sass'
 end
 
-execute "mkdir-lib" do
-    not_if {File.exists?(lib)}
-    command "mkdir #{lib}"
+directory node[:sass][:lib] do
+    not_if File.exists?(node[:sass][:lib])
+    owner node[:sass][:user]
+    group node[:sass][:group]
+    action :create
 end
 
-execute "mkdir-sass" do
-    not_if {File.exists?(sassPath)}
-    command "mkdir #{sassPath}"
+directory "#{node[:sass][:lib]}/sass" do
+    not_if File.exists?("#{node[:sass][:lib]}/sass")
+    owner node[:sass][:user]
+    group node[:sass][:group]
+    action :create
 end
 
-execute "create-procfile" do
-    command "nohup sass --watch #{sassPath}:#{lib} &"
+execute "Start SASS conversion" do
+    command "nohup sass --watch #{node[:sass][:lib]/sass}:#{node[:sass][:lib]} &"
 end
-
-# execute 'install-foreman' do
-#     command 'gem install foreman'
-# end
-
-# execute "start-foreman" do
-#     command "foreman start -f /home/vagrant/Procfile &"
-# end
-
-# execute "create-sass-service" do
-#     command "echo 'sass --watch #{sassPath}:#{lib}'" >> "~/sass.sh"
-# end
-
-# execute 'start-sass-watch' do
-#     command "chmod a+x ~/sass.sh && ~/sass.sh &"
-# end

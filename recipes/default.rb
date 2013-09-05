d8d42ae00664826d21a4c604fae6408acad3f862
Jeremy Seago <seagoj@gmail.com>
2013-09-05 14:07:09 -0500

    Adds test.scss to sass dir

--- recipes/default.rb
+++ recipes/default.rb
@@ -18,6 +18,12 @@ directory "#{node[:sass][:lib]}/sass" do
     action :create
 end
 
+cookbook_file "#{node[:sass][:lib]}/sass/test.scss" do
+    not_if File.exists?("#{node[:sass][:lib]}/sass/test.scss"
+    owner node[:sass][:user]
+    group node[:sass][:group]
+end
+
 execute "Start SASS conversion" do
     command "nohup sass --watch #{node[:sass][:lib]/sass}:#{node[:sass][:lib]} &"
 end

4fc296d394002e3fcf72d5304b4fcecfe4a8240f
Jeremy Seago <seagoj@gmail.com>
2013-09-06 12:47:09 -0500

    Update default.rb

--- recipes/default.rb
+++ recipes/default.rb
@@ -5,21 +5,21 @@ execute 'install-sass' do
 end
 
 directory node[:sass][:lib] do
-    not_if File.exists?(node[:sass][:lib])
+    not_if {File.exists?(node[:sass][:lib])}
     owner node[:sass][:user]
     group node[:sass][:group]
     action :create
 end
 
 directory "#{node[:sass][:lib]}/sass" do
-    not_if File.exists?("#{node[:sass][:lib]}/sass")
+    not_if {File.exists?("#{node[:sass][:lib]}/sass")}
     owner node[:sass][:user]
     group node[:sass][:group]
     action :create
 end
 
 cookbook_file "#{node[:sass][:lib]}/sass/test.scss" do
-    not_if File.exists?("#{node[:sass][:lib]}/sass/test.scss")
+    not_if {File.exists?("#{node[:sass][:lib]}/sass/test.scss")}
     owner node[:sass][:user]
     group node[:sass][:group]
 end
